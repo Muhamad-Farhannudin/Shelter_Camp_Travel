3ebe0968a33cf9268333ca11a7ee5c73d21ab7c5
Muhamad-Farhannudin <110012265+Muhamad-Farhannudin@users.noreply.github.com>
2023-09-23 06:46:02 +0700

    completed debugging error in website staycation

--- src/parts/Activities.js
+++ src/parts/Activities.js
@@ -39,7 +39,6 @@ export default function Activities({ data }) {
                     <div className="meta-wrapper">
                       <Button
                         type="link"
-                        href={`/properties/${item._id}`}
                         className="stretched-link d-block text-gray-800"
                       >
                         <h5 className="h4">{item.name}</h5>
